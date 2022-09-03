f7007845c22ee826e3d276c1698155248343d585
tessla-muir <tesslamuir@yahoo.com>
2022-09-03 14:18:29 -0400

    procedural generation of platforms

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,21 +7,71 @@ public class LevelGenerator : MonoBehaviour
     public GameObject platformPrefab;
     public Transform player;
 
-    public int numberOfPlatforms = 200;
-    public float levelWidth = 5f;
-    public float minY =.2f;
-    public float maxY = 1.5f;
 
+    private int platformCount = 2;
+    public int maxPlatforms = 18;
+    private float maxHeight = 0;
+    private float lastPlatformHeight = 0;
+
+    public float levelWidth = 5f;
+    private float minY = .2f;
+    private float maxY = 1.2f;
+
+    private GameObject[] platforms;
 
     void Start()
     {
         Vector3 spawnPosition = new Vector3();
 
-        for (int i = 0; i < numberOfPlatforms; i++) 
+        for (int i = platformCount; i < maxPlatforms; i++)
         {
             spawnPosition.y += Random.Range(minY, maxY);
             spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+
             Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            Debug.Log("Platforms: " + platformCount);
+            platformCount++;
+
+            if (spawnPosition.y > lastPlatformHeight) {
+                lastPlatformHeight = spawnPosition.y;
+            }
+        }
+    }
+
+    void Update() 
+    {
+        // Updates maxHeight player has reached
+        if (player.position.y > maxHeight) 
+        {
+            maxHeight = player.position.y;
+        }
+
+        platforms = GameObject.FindGameObjectsWithTag("platform");
+
+        // Deletes old platforms
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            if (platforms[i].transform.position.y + 5f < player.position.y)
+            {
+                Destroy(platforms[i]);
+                platformCount--;
+            }
+        }
+    }
+
+    void LateUpdate() 
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        // Adds new platform
+        if (platformCount < maxPlatforms)
+        {
+            spawnPosition.y += Random.Range(lastPlatformHeight + .8f, lastPlatformHeight + 1.4f);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            lastPlatformHeight = spawnPosition.y;
+
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            platformCount++;
         }
     }
 }
